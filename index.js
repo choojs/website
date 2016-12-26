1e13f4f73229645f2d8c58def6eaccafe8d40d52
Yoshua Wuyts <yoshuawuyts@gmail.com>
2016-12-26 20:24:16 +0100

    footer & header

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ function mainView () {
       ${Main()}
       ${Description()}
       ${Example()}
+      ${Principles()}
+      ${Footer()}
     </body>
   `
 }
@@ -39,24 +41,16 @@ function Main () {
       </div>
       <div class="f5 lh-copy fl w-100 mb4">
         <div class="fl-ns dib w-100 w-20-l pr3-m pr4-l">
-          <a class="black link" href="/">
-            Navigate Home
-          </a>
+          tiny API
         </div>
         <div class="fl-ns w-100 w-20-l pr3-m pr4-l">
-          <a class="black link" href="https://github.com/yoshuawuyts/choo-handbook">
-            View the handbook
-          </a>
+          performs well
         </div>
         <div class="fl-ns w-100 w-20-l pr3-m pr4-l">
-          <a class="black link" href="https://github.com/trainyard/choo-cli">
-            Install the CLI
-          </a>
+          5kb in size
         </div>
         <div class="fl-ns w-100 w-20-l pr3-m pr4-l">
-          <a class="black link" href="https://github.com/yoshuawuyts/choo">
-            Visit GitHub
-          </a>
+          v impressive
         </div>
       </div>
     </main>
@@ -91,5 +85,102 @@ function Description () {
         ${fs.readFileSync(path.join(__dirname, 'assets/intro.txt'), 'utf8')}
       </p>
     </section>
+  `
+}
+
+function Principles () {
+  return html`
+    <section class="tl pa4 pa5-l bg-lightest-blue navy">
+      <div class="mw9 center">
+        <h1 class="f2 lh-title fw9 mb3 mt0 pt3 bt bw2">
+          Principles
+        </h1>
+        <section class="lh-copy">
+          <div class="cf">
+            <article class="fl pv2 w-100 w-third-l pr4-l">
+              <h2 class="f5 f4-ns fw6 mb0">Responsive</h2>
+              <p class="f6 f5-ns measure lh-copy mt0">
+                Everything should be 100% responsive. Your website should work regardless of a users
+                device or screensize.
+              </p>
+            </article>
+            <article class="pv2 fl w-100 w-third-l ph3-l">
+              <h2 class="f5 f4-ns fw6 mb0">Readable</h2>
+              <p class="f6 f5-ns measure lh-copy mt0">
+                No matter the lighting, or the device, font-sizes should be
+                large enough and contrast should be high enough for your
+                users to easily read your content.
+              </p>
+            </article>
+            <article class="pv2 fl w-100 w-third-l pl4-l">
+              <h2 class="f5 f4-ns  fw6 mb0">
+                Modular
+              </h2>
+              <p class="f6 f5-ns measure lh-copy mt0">
+                Tachyons isn't just a monolithic framework. It's a collection of small modules
+                that can be mixed and matched or used independently. Use what you need. Leave the rest.
+              </p>
+            </article>
+          </div>
+          <div class="cf w-100">
+            <article class="pv2 fl w-100 w-third-l pl0 pr4-l">
+              <h2 class="f5 f4-ns fw6 mb0">Accessible</h2>
+              <p class="f6 f5-ns measure lh-copy mt0">
+                Accessibility is important to us. Throughout both the css
+                and the documentation we provide numerous tools and methods for making it
+                easier to maximize the accessibility of your site.
+              </p>
+            </article>
+            <article class="pv2 fl w-100 w-third-l ph3-l">
+              <h2 class="f5 f4-ns  fw6 mb0">Performant</h2>
+              <p class="f6 f5-ns measure lh-copy mt0">
+                Code should be optimized for performance.
+              </p>
+            </article>
+            <article class="pv2 fl w-100 w-third-l pl4-l">
+              <h2 class="f5 f4-ns fw6 mb0">
+                Reusable
+              </h2>
+              <p class="f6 f5-ns measure lh-copy mt0">
+                Clear documentation helps create a shared understanding of design patterns amongst your team.
+                This helps promote reuse and reduces the amount of redundancy in a codebase.
+              </p>
+            </article>
+          </div>
+        </section>
+      </div>
+    </section>
+  `
+}
+
+function Footer () {
+  return html`
+    <footer class="bg-white ph4 ph5-l pb4 pt4 pt5-l">
+      <div class="f5 lh-copy fl w-100">
+        <div class="fl-ns dib w-100 w-20-l pr3-m pr4-l b">
+          <a class="black link" href="/">
+            Navigate Home
+          </a>
+        </div>
+        <div class="fl-ns w-100 w-20-l pr3-m pr4-l b">
+          <a class="black link" href="https://github.com/yoshuawuyts/choo-handbook">
+            View the handbook
+          </a>
+        </div>
+        <div class="fl-ns w-100 w-20-l pr3-m pr4-l b">
+          <a class="black link" href="https://github.com/trainyard/choo-cli">
+            Install the CLI
+          </a>
+        </div>
+        <div class="fl-ns w-100 w-20-l pr3-m pr4-l b">
+          <a class="black link" href="https://github.com/yoshuawuyts/choo">
+            Visit GitHub
+          </a>
+        </div>
+      </div>
+      <p class="b pt6 pt5-l">
+        Made with ♥️ in Saigon, Tokyo, Berlin
+      </p>
+    </footer>
   `
 }
