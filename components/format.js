91880bf7f45284ae5ba420626d962702861f8c66
Renée Kooi <renee@kooi.me>
2018-01-29 10:21:46 +0100

    Remove unused highlight.js languages from bundle (#48)
    highlight.js ships with a [massive](https://unpkg.com/highlight.js@9.12.0/lib/index.js)
    amount of languages by default. this PR replaces
    `markdown-it-highlightjs` with a custom version that only includes
    highlighting for xml (html), css, javascript and markdown (not sure if
    we use that last one?)
    
    Before:
    
        232K bundle.js.br
    
    After:
    
        65K bundle.js.br

--- components/format.js
+++ components/format.js
@@ -1,13 +1,44 @@
 var Markdown = require('markdown-it')
+var hljs = require('highlight.js/lib/highlight')
 var raw = require('bel/raw')
 var md = Markdown({
   html: true
 })
 
-md.use(require('markdown-it-highlightjs'))
+hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'))
+hljs.registerLanguage('css', require('highlight.js/lib/languages/css'))
+hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'))
+hljs.registerLanguage('markdown', require('highlight.js/lib/languages/markdown'))
+
+md.use(highlighter)
 
 module.exports = format
 
 function format (str) {
   return raw(md.render(str || ''))
+}
+
+function highlighter (md, opts) {
+  md.options.highlight = highlight
+  var rules = md.renderer.rules
+  rules.fence = wrap(rules.fence)
+  rules.code_block = wrap(rules.code_block)
+
+  function highlight (code, lang) {
+    var result = null
+    if (lang && hljs.getLanguage(lang)) {
+      result = hljs.highlight(lang, code, true)
+    } else {
+      result = hljs.highlightAuto(code)
+    }
+    return result ? result.value : ''
+  }
+
+  function wrap (render) {
+    return function () {
+      return render.apply(this, arguments)
+        .replace(/<code class="/g, '<code class="hljs ')
+        .replace(/<code>/g, '<code class="hljs">')
+    }
+  }
 }
